d9aedaed17edf49be5c8287af45dafe699f7a9a9
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-10 19:18:14 +0600

    Update twoSumSortedArray.cpp

--- Sum/twoSumSortedArray.cpp
+++ Sum/twoSumSortedArray.cpp
@@ -52,3 +52,30 @@ int main() {
 
     return 0;
 }
+//O(n) - two pointer approach
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> result;
+        int start = 0;
+        int end = numbers.size() -1;
+        while (start < end) {
+            if (numbers[start] + numbers[end]< target){
+                start++;
+            }
+            else if (numbers[start] + numbers[end] > target){
+                end--;
+            }
+            else {
+                result.push_back(start +1);
+                result.push_back(end +1);
+                break;
+            }
+        }
+
+        return result;
+
+    }
+};
